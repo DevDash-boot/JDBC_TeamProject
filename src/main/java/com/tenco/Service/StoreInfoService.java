a62deed91dbbc3ef10572219fcd318f861b7a112
DevDash <jeonghs1570@gmail.com>
2026-09-16 22:43:20 +0900

    Update StoreInfoService.java

--- src/main/java/com/tenco/Service/StoreInfoService.java
+++ src/main/java/com/tenco/Service/StoreInfoService.java
@@ -1,65 +1,22 @@
-package com.tenco.dao;
+package com.tenco.Service;
 
+import com.tenco.dao.StoreInfoDAO;
 import com.tenco.dto.StoreInfo;
-import com.tenco.util.util;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class StoreInfoDAO {
-
-    // 매점 전체 조회
+public class StoreInfoService {
+    private final StoreInfoDAO storeInfoDAO = new StoreInfoDAO();
+    // 매장 전체 조회
     public List<StoreInfo> allStoreInfo(){
-        List<StoreInfo> storeInfoList = new ArrayList<>();
-        String sql = """
-                SELECT * FROM store;
-                """;
-        try (Connection conn = util.getConnection()) {
-            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                try (ResultSet rs = pstmt.executeQuery()) {
-                    printStore(rs, storeInfoList);
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return storeInfoList;
+        return storeInfoDAO.allStoreInfo();
     }
 
-    // 매점 지역별 조회
+    // 선택 매장 조회
     public List<StoreInfo> LocationStore(String location){
-        List<StoreInfo> storeInfoList = new ArrayList<>();
-        String sql = """
-                SELECT * FROM store
-                WHERE location = ? ;
-                """;
-        try (Connection conn = util.getConnection()) {
-            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                pstmt.setString(1, location);
-                try (ResultSet rs = pstmt.executeQuery()) {
-                    printStore(rs, storeInfoList);
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if(location == null || location.trim().isEmpty()){
+            System.out.println("찾으실 매장 위치를 입력해주세요.");
         }
-        return storeInfoList;
-    }
-
-    private static void printStore(ResultSet rs, List<StoreInfo> storeInfoList) throws SQLException {
-        while(rs.next()){
-            storeInfoList.add(StoreInfo.builder()
-                    .storeId(rs.getInt("store_id"))
-                    .storeName(rs.getString("store_name"))
-                    .openTime(rs.getTime("open_time").toLocalTime())
-                    .closeTime(rs.getTime("close_time").toLocalTime())
-                    .storeTell(rs.getString("store_tell"))
-                    .Location(rs.getString("location"))
-                    .build());
-        }
+        return storeInfoDAO.LocationStore(location);
     }
 }
