3b8a23bc8e4729fda14227313c83156cc5d0b087
MIKY-1 <yuyn2000@gmail.com>
2026-09-17 19:17:44 +0900

    .

--- src/main/java/com/tenco/view/PurchaseView.java
+++ src/main/java/com/tenco/view/PurchaseView.java
@@ -81,7 +81,7 @@ public class PurchaseView {
         public void purchaseAllView() throws SQLException {
             for (PurchaseDto p : purchaseService.getAllPurchases()) {
                 System.out.printf("발주 ID : %d , 상품 ID : %d , 상품명 : %s , 발주 수량 : %d , 개당 상품가격 : %d , 총 발주가격 : %d\n" ,
-                        p.getPurchaseId(), p.getProductId() , p.getName() , p.getQauntity() , p.getUnitPrice() , p.getTotlaPrice());
+                        p.getPurchaseId(), p.getProduct_id() , p.getName() , p.getQauntity() , p.getUnitPrice() , p.getTotlaPrice());
                 System.out.println();
             }
         }
@@ -90,6 +90,6 @@ public class PurchaseView {
     public void purchaseSingleView(int id) throws Exception {
         PurchaseDto p = purchaseService.existList(id);
         System.out.printf(" 발주 ID : %d , 상품 ID : %d , 상품명 : %s , 발주 수량 : %d , 개당 상품가격 : %d , 총 발주가격 : %d\n" ,
-                p.getPurchaseId() , p.getProductId() , p.getName() , p.getQauntity() , p.getUnitPrice() , p.getTotlaPrice());
+                p.getPurchaseId() , p.getProduct_id() , p.getName() , p.getQauntity() , p.getUnitPrice() , p.getTotlaPrice());
         }
     }
